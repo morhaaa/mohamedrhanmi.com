a71777f29a1031058af12ea5b25d68896d3bac87
Mohamed <mohamed.rhanmi@gmail.com>
2023-12-09 15:43:24 +0100

    Create section divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-4 pt-36">
+    <main className="flex flex-col items-center  px-4 pt-36">
       <Intro />
+      <SectionDivider />
     </main>
   );
 }
