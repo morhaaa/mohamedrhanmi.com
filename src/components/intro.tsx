9833ca4773da24f1a0a7be9651149906d4af73fa
Mohamed <mohamed.rhanmi@gmail.com>
2023-12-09 15:34:00 +0100

    Fix intro links

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -86,7 +86,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/mohamed-rhanmi/"
           target="_blank"
         >
           <BsLinkedin />
@@ -94,7 +94,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/morhaaa"
           target="_blank"
         >
           <FaGithubSquare />
