b04498dce844d0790cdd1afd664f349b93a7dfbb
Mohamed <mohamed.rhanmi@gmail.com>
2023-12-09 15:23:43 +0100

    Create intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Intro from "@/components/intro";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="flex min-h-screen flex-col items-center justify-between px-4 pt-36">
+      <Intro />
     </main>
-  )
+  );
 }
---
+++ src/components/intro.tsx
@@ -0,0 +1,105 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="/memoji.svg"
+              width="192"
+              height="192"
+              alt="portrait"
+              quality="95"
+              priority={true}
+              className="rounded-full object-cover shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="text-6xl absolute bottom-0 left-0"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              damping: 10,
+              duration: 0.2,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        Hello, I'm <span className="font-bold">Mohamed Rhanmi</span>, a{" "}
+        <span className="font-bold">front-end developer</span>. I'm specialized
+        in building dynamic and user-friendly
+        <span className="italic">websites and applications</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.h1>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          onClick={() => {}}
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          href="/cv.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://linkedin.com"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
