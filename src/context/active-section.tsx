c085d723c2212c23f23b7480ccc9b9bd360fc6fd
Mohamed <mohamed.rhanmi@gmail.com>
2023-12-11 08:46:20 +0100

    Create useInView hook

--- src/context/active-section.tsx
+++ src/context/active-section.tsx
@@ -39,5 +39,12 @@ export default function ActiveSectionContextProvider({
 
 export function useActiveSectionContext() {
   const context = useContext(ActiveSectionContext);
+
+  if (context === null) {
+    throw new Error(
+      "useActiveSectionContext must be used within an ActiveSectionContextProvider"
+    );
+  }
+
   return context;
 }
