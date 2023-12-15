845039d2bab8e229561cd0fd7cfaee09cbaa7ed1
Mohamed <mohamed.rhanmi@gmail.com>
2023-12-15 17:38:00 +0100

    Set language projects component

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -5,13 +5,18 @@ import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project-card";
 import { useSectionInView } from "@/lib/useInView";
+import { useLanguage } from "@/containers/language-context";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
 
+  const { activeLanguage } = useLanguage();
+
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
-      <SectionHeading>My projects</SectionHeading>
+      <SectionHeading>
+        {activeLanguage === "It" ? "I miei lavori" : "My works"}
+      </SectionHeading>
       <div>
         {projectsData.map((project, index) => (
           <Project {...project} key={index} />
