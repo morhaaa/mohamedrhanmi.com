4ac3750c820f912c94d276b0b4347be23f6af206
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-03-25 07:26:54 +0100

    chore: set light theme as default

--- src/containers/theme-context.tsx
+++ src/containers/theme-context.tsx
@@ -18,7 +18,7 @@ const ThemeContext = createContext<ThemeContextType | null>(null);
 export default function ThemeContextProvider({
   children,
 }: ThemeContextProviderProps) {
-  const [theme, setTheme] = useState<Theme>("dark");
+  const [theme, setTheme] = useState<Theme>("light");
 
   const toggleTheme = () => {
     if (theme === "light") {
